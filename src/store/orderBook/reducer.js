b62d0939d11acfd9298137547c39837194011c47
marcelocarmona <marcelo@marcelocarmona.com>
2018-12-17 03:36:17 -0300

    add BOOK_UPDATE_MESSAGE

--- src/store/orderBook/reducer.js
+++ src/store/orderBook/reducer.js
@@ -9,17 +9,19 @@ function book(state = initialState, action) {
   switch (action.type) {
     case BOOK_SNAPSHOT_MESSAGE:
       const books = action.payload[1];
-      return books.reduce((acc, [price, count, amount], currentIndex) => {
-        return currentIndex < 25
-          ? {
-              ...acc,
-              asks: { ...acc.asks, [price]: { amount, count, price } }
-            }
-          : {
-              ...acc,
-              bids: { ...acc.bids, [price]: { amount, count, price } }
-            };
-      }, initialState);
+      return books.reduce(
+        (acc, [price, count, amount], currentIndex) =>
+          currentIndex < 25
+            ? {
+                ...acc,
+                asks: { ...acc.asks, [price]: { amount, count, price } }
+              }
+            : {
+                ...acc,
+                bids: { ...acc.bids, [price]: { amount, count, price } }
+              },
+        initialState
+      );
     case BOOK_UPDATE_MESSAGE:
       /*
         when count > 0 then you have to add or update the price level
@@ -29,6 +31,38 @@ function book(state = initialState, action) {
         4.1 if amount = 1 then remove from bids
         4.2 if amount = -1 then remove from asks
       */
+      const [price, count, amount] = action.payload[1];
+      if (count > 0) {
+        if (amount > 0) {
+          return {
+            ...state,
+            bids: { ...state.bids, [price]: { amount, count, price } }
+          };
+        }
+        if (amount < 0) {
+          return {
+            ...state,
+            asks: { ...state.asks, [price]: { amount, count, price } }
+          };
+        }
+      } else if (count === 0) {
+        if (amount === 1) {
+          const bidsCopy = { ...state.bids };
+          delete bidsCopy[price];
+          return {
+            ...state,
+            bids: bidsCopy
+          };
+        }
+        if (amount === -1) {
+          const asksCopy = { ...state.asks };
+          delete asksCopy[price];
+          return {
+            ...state,
+            asks: asksCopy
+          };
+        }
+      }
       return {
         ...state
       };
