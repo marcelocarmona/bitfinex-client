2031de45ae969e34957d0a4cf35e035d91c24f82
marcelocarmona <marcelo@marcelocarmona.com>
2018-12-18 02:11:14 -0300

    Updates

---
+++ precache-manifest.0d1c1bc634d84ef87db33e62bbffc917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e57d90104682e6e1490c",
+    "url": "/static/js/main.e57d9010.chunk.js"
+  },
+  {
+    "revision": "be85f3065a3ec51849f0",
+    "url": "/static/js/1.be85f306.chunk.js"
+  },
+  {
+    "revision": "e57d90104682e6e1490c",
+    "url": "/static/css/main.16ac9efe.chunk.css"
+  },
+  {
+    "revision": "ae2c1770f357671096f4b2b83bc08301",
+    "url": "/index.html"
+  }
+];
